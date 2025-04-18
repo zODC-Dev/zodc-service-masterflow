4fe567634b6d91611758fbd74aa646ab411fa2cd
ThanhF <trthanh179@gmail.com>
2025-04-18 14:46:56 +0700

    [ZODC-XXX][1.1.21] RECALCULATE REQUETS PROGRESS IN APPROVE REJECT NODE

--- internal/app/services/node_service.go
+++ internal/app/services/node_service.go
@@ -88,7 +88,7 @@ func (s *NodeService) CheckIfAllNodeFormIsApprovedOrRejected(ctx context.Context
 
 func (s *NodeService) LogicForConditionNode(ctx context.Context, tx *sql.Tx, nodeId string, isTrue bool, userId int32) error {
 	// Update Node Form
-	node, err := s.NodeRepo.FindOneNodeByNodeId(ctx, s.DB, nodeId)
+	node, err := s.NodeRepo.FindOneNodeByNodeIdTx(ctx, tx, nodeId)
 	if err != nil {
 		return err
 	}
@@ -732,6 +732,32 @@ func (s *NodeService) ApproveNode(ctx context.Context, userId int32, nodeId stri
 		return fmt.Errorf("logic for condition node fail: %w", err)
 	}
 
+	request, err := s.RequestRepo.FindOneRequestByRequestIdTx(ctx, tx, node.RequestID)
+	if err != nil {
+		return fmt.Errorf("find request by node id fail: %w", err)
+	}
+
+	isRequestCompleted := false
+	for _, node := range request.Nodes {
+		if node.Type == string(constants.NodeTypeEnd) && node.Status == string(constants.NodeStatusCompleted) {
+			isRequestCompleted = true
+			break
+		}
+	}
+
+	if isRequestCompleted {
+		request.Progress = 100
+		requestModel := model.Requests{}
+		utils.Mapper(request, &requestModel)
+		if err := s.RequestRepo.UpdateRequest(ctx, tx, requestModel); err != nil {
+			return fmt.Errorf("update request progress fail: %w", err)
+		}
+	} else {
+		if err := s.RequestService.UpdateCalculateRequestProgress(ctx, tx, node.RequestID); err != nil {
+			return fmt.Errorf("update calculate request progress fail: %w", err)
+		}
+	}
+
 	// Commit
 	if err := tx.Commit(); err != nil {
 		return err
@@ -768,6 +794,32 @@ func (s *NodeService) RejectNode(ctx context.Context, userId int32, nodeId strin
 	err = s.LogicForConditionNode(ctx, tx, nodeId, false, userId)
 	if err != nil {
 		return fmt.Errorf("logic for condition node fail: %w", err)
+	}
+
+	request, err := s.RequestRepo.FindOneRequestByRequestIdTx(ctx, tx, node.RequestID)
+	if err != nil {
+		return fmt.Errorf("find request by node id fail: %w", err)
+	}
+
+	isRequestCompleted := false
+	for _, node := range request.Nodes {
+		if node.Type == string(constants.NodeTypeEnd) && node.Status == string(constants.NodeStatusCompleted) {
+			isRequestCompleted = true
+			break
+		}
+	}
+
+	if isRequestCompleted {
+		request.Progress = 100
+		requestModel := model.Requests{}
+		utils.Mapper(request, &requestModel)
+		if err := s.RequestRepo.UpdateRequest(ctx, tx, requestModel); err != nil {
+			return fmt.Errorf("update request progress fail: %w", err)
+		}
+	} else {
+		if err := s.RequestService.UpdateCalculateRequestProgress(ctx, tx, node.RequestID); err != nil {
+			return fmt.Errorf("update calculate request progress fail: %w", err)
+		}
 	}
 
 	if err := tx.Commit(); err != nil {
